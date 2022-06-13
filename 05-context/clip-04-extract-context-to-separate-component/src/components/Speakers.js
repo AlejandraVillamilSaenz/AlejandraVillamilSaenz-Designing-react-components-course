c7f74394296af258f8685f4bf7a5e777697cfc4b
AlejandraVillamilSaenz <98572330+AlejandraVillamilSaenz@users.noreply.github.com>
2022-06-13 07:29:19 -0500

    Add files via upload

---
+++ 05-context/clip-04-extract-context-to-separate-component/src/components/Speakers.js
@@ -0,0 +1,16 @@
+import SpeakersToolbar from "./SpeakersToolbar";
+import SpeakersList from "./SpeakersList";
+import { useState } from "react";
+function Speakers() {
+  const [showSessions, setShowSessions] = useState(true);
+  return (
+    <>
+      <SpeakersToolbar
+        showSessions={showSessions}
+        setShowSessions={setShowSessions}
+      />
+      <SpeakersList showSessions={showSessions} />
+    </>
+  );
+}
+export default Speakers;
